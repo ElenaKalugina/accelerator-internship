d7903f2bc26d7164a8d4e89efcff338d6c197588
Elena Kalugina <rena6@yandex.ru>
2024-01-19 13:02:09 +0300

    Доделала меню

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,11 @@
 // https://swiperjs.com/get-started#installation
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
-
+// // import 'swiper/css';
+// import './utils/scroll-lock';
+// import './utils/focus-lock';
 import {iosVhFix} from './utils/ios-vh-fix';
-import {menuToggle} from './modules/menu/menu-toggle.js';
+import {menuToggle} from './modules/menu-toggle.js';
 import {initAccordions} from './vendor/accordions/init-accordion';
 
 window.addEventListener('DOMContentLoaded', () => {
--- source/js/modules/menu-toggle.js
+++ source/js/modules/menu-toggle.js
@@ -1,7 +1,10 @@
+import '../utils/scroll-lock';
+import '../utils/focus-lock';
 const isMenuOpened = 'is-open';
-const menuButton = document.querySelector('[data-menu-btn="btn-toggle"]');
-const menu = document.querySelector('[data-menu="menu"]');
+const menuButton = document.querySelector('[data-nav="btn-toggle"]');
+const menu = document.querySelector('[data-nav="menu"]');
 const overlay = document.querySelector('[data-overlay="overlay"]');
+const nav = document.querySelector('[data-nav="nav"]');
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
@@ -17,20 +20,34 @@ const onWindowKeyDown = (evt) => {
   }
 };
 
+const onMenuLinkClick = (evt) => {
+  if (evt.target.matches('[data-nav="menu-link"]')) {
+    closeMenu();
+  }
+};
+
 function closeMenu () {
   menuButton.classList.remove(isMenuOpened);
   menu.classList.remove(isMenuOpened);
   overlay.classList.remove(isMenuOpened);
+  nav.classList.remove(isMenuOpened);
   document.removeEventListener('keydown', onWindowKeyDown);
   overlay.removeEventListener('click', onOverlayClick);
+  nav.removeEventListener('click', onMenuLinkClick);
+  window.scrollLock.enableScrolling();
+  window.focusLock.unlock();
 }
 
 function openMenu () {
   menuButton.classList.add(isMenuOpened);
   menu.classList.add(isMenuOpened);
   overlay.classList.add(isMenuOpened);
+  nav.classList.add(isMenuOpened);
   document.addEventListener('keydown', onWindowKeyDown);
   overlay.addEventListener('click', onOverlayClick);
+  nav.addEventListener('click', onMenuLinkClick);
+  window.scrollLock.disableScrolling();
+  window.focusLock.lock('[data-menu="menu"]', false);
 }
 
 const onMenuButtonClick = () => {
