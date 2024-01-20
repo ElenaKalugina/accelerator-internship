8be0b9394849c789f65cada73fa9f9d9bf4c309f
Elena Kalugina <rena6@yandex.ru>
2024-01-20 17:35:40 +0300

    Добавила 1 слайд для всех vp

--- source/js/main.js
+++ source/js/main.js
@@ -2,8 +2,7 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // // import 'swiper/css';
-// import './utils/scroll-lock';
-// import './utils/focus-lock';
+
 import {iosVhFix} from './utils/ios-vh-fix';
 import {menuToggle} from './modules/menu-toggle.js';
 import {initAccordions} from './vendor/accordions/init-accordion';
