d321982a974882bd7326a69fca4f1e41db0ff560
Elena Kalugina <rena6@yandex.ru>
2024-01-31 22:00:43 +0300

    Добавила форму + JS + тесты

--- source/js/main.js
+++ source/js/main.js
@@ -1,15 +1,12 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// // import 'swiper/css';
-
 import {iosVhFix} from './utils/ios-vh-fix';
+import {Form} from './vendor/form-validate/form';
 import {menuToggle} from './modules/menu-toggle.js';
 import {createSliderHero} from './modules/hero-slider.js';
 import {initAccordions} from './vendor/accordions/init-accordion';
 import {createSliderPrograms} from './modules/programs-slider.js';
 import {createSliderNews} from './modules/news-slider.js';
 import {createReviewsSwiper} from './modules/reviews-slider.js';
+import {CustomSelect} from './vendor/custom-select/custom-select';
 
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
@@ -21,5 +18,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
   window.addEventListener('load', () => {
     initAccordions();
+
+    const select = new CustomSelect();
+    select.init();
+
+    const form = new Form();
+    window.form = form;
+    form.init();
   });
 });
